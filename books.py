2524298fc5e87bf31f8af170b75848bca005e8da
punkushal <spycymagar@gmail.com>
2025-01-07 14:02:00 +0545

    enhanced request method

--- books.py
+++ books.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def first_api():
-    return {'message':'Hello Kushal'}
+BOOKS = [
+    {'title': 'first title','category':'science'},
+    {'title': 'second title','category':'tech'},
+    {'title': 'third title','category':'science'},
+    {'title': 'fourth title','category':'history'},
+    {'title': 'fivth title','category':'education'},
+]
+
+@app.get('/books')
+def read_all_books():
+    return BOOKS
