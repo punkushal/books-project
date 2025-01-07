c190e6ba040491dc63c0a5c9f9646fe9c45f90cf
punkushal <spycymagar@gmail.com>
2025-01-07 14:29:50 +0545

    get book by title using path parameter concept

--- books.py
+++ books.py
@@ -14,6 +14,14 @@ BOOKS = [
 def read_all_books():
     return BOOKS
 
+#getting book with title name which will be path parameter
+@app.get('/books/{book_title}')
+def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
 @app.get('/books/{dynamic_param}')
 def read_all_books(dynamic_param):
     return {'dynamic':dynamic_param}
