807ba972f0c5ddd1d4085d3bf240c1e6872f966b
punkushal <spycymagar@gmail.com>
2025-01-16 12:55:13 +0545

    get all todos from database table

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
-from database import engine
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from database import engine, sessionLocal
+from sqlalchemy.orm import Session
 import models
-
 app = FastAPI()
 
 models.Base.metadata.create_all(bind= engine)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get('/')
+def get_all(db : Annotated[Session, Depends(get_db)]):
+    return db.query(models.Todos).all()
